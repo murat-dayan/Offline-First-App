64985e30838e2fa6f70e77681e720300e52d8dca
muratdayan <muratdayan359@gmail.com>
2024-11-10 18:38:08 +0300

    package structure builded

--- app/src/main/java/com/muratdayan/offlinenewsapp/core/presentation/MainActivity.kt
+++ app/src/main/java/com/muratdayan/offlinenewsapp/core/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.muratdayan.offlinenewsapp
+package com.muratdayan.offlinenewsapp.core.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.muratdayan.offlinenewsapp.ui.theme.OfflineNewsAppTheme
+import com.muratdayan.offlinenewsapp.core.presentation.ui.theme.OfflineNewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/muratdayan/offlinenewsapp/core/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/muratdayan/offlinenewsapp/core/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.muratdayan.offlinenewsapp.ui.theme
+package com.muratdayan.offlinenewsapp.core.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
